c3438afed000b22da544c5d18624a810f8dc642a
dazzle50 <dazzle50@gmail.com>
2025-08-13 09:21:23 +0100

    Release v0.1.2 Misc general improvements to Utils including using StackWalker to get thread stack details

--- src/rjc/table/Utils.java
+++ src/rjc/table/Utils.java
@@ -18,11 +18,13 @@
 
 package rjc.table;
 
+import java.lang.StackWalker.StackFrame;
 import java.lang.reflect.Array;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /*************************************************************************************************/
 /******************* Miscellaneous utility public static methods and variables *******************/
@@ -30,67 +32,83 @@ import java.util.List;
 
 public class Utils
 {
-  public static final String      VERSION            = "v0.1.2 WIP";
+  public static final String             VERSION      = "v0.1.2";
 
-  public static DateTimeFormatter timestampFormatter = DateTimeFormatter.ofPattern( "uuuu-MM-dd HH:mm:ss.SSS " );
+  // pre-compiled formatter and pattern for better performance
+  private static final DateTimeFormatter TIMESTAMP    = DateTimeFormatter.ofPattern( "uuuu-MM-dd HH:mm:ss.SSS " );
+  private static final Pattern           WHITESPACE   = Pattern.compile( "(\\s+)" );
+
+  // stack walking configuration
+  private static final String            EARLY_EXIT   = "com.sun.javafx.application.LauncherImpl";
+  private static final String            SKIP_ISIGNAL = "rjc.table.signal.ISignal$SignalHelper";
+  private static final StackWalker       STACK_WALKER = StackWalker.getInstance();
 
   /****************************************** timestamp ******************************************/
   public static String timestamp()
   {
     // returns current date-time as string in format YYYY-MM-DD HH:MM:SS.SSS
-    return LocalDateTime.now().format( timestampFormatter );
+    return LocalDateTime.now().format( TIMESTAMP );
   }
 
   /******************************************** trace ********************************************/
   public static void trace( Object... objects )
   {
     // sends to standard out date-time, the input objects, suffixed by file+line-number & method
-    System.out.println( timestamp() + objectsString( objects ) + caller( 1 ) );
+    String caller = STACK_WALKER.walk( stream ->
+    {
+      var iterator = stream.iterator();
+      iterator.next(); // skip trace()
+      return stackFrameString( iterator.next() );
+    } );
+    System.out.println( timestamp() + objectsString( objects ) + caller );
   }
 
   /********************************************* path ********************************************/
   public static void path( Object... objects )
   {
-    final String EARLY_EXIT = "com.sun.javafx.application.LauncherImpl";
-    final String SKIP_ISIGNAL = "rjc.table.signal.ISignal$SignalHelper";
-
     // sends to standard out date-time, the input objects, and simplified stack path
-    StackTraceElement[] stack = new Throwable().getStackTrace();
-    StringBuilder str = new StringBuilder();
-    for ( int i = 1; i < stack.length; i++ )
+    String path = STACK_WALKER.walk( stream ->
     {
-      String name = stack[i].getClassName();
-      if ( name == EARLY_EXIT )
-        break;
-      if ( name == SKIP_ISIGNAL )
-        i++;
-      else
-        str.append( caller( i ) );
-    }
-    System.out.println( timestamp() + objectsString( objects ) + str );
+      StringBuilder str = new StringBuilder();
+      var iterator = stream.iterator();
+      iterator.next(); // skip path()
+
+      while ( iterator.hasNext() )
+      {
+        StackFrame frame = iterator.next();
+        String className = frame.getClassName();
+        if ( EARLY_EXIT.equals( className ) )
+          break;
+        if ( !SKIP_ISIGNAL.equals( className ) )
+          str.append( stackFrameString( frame ) );
+      }
+      return str.toString();
+    } );
+    System.out.println( timestamp() + objectsString( objects ) + path );
   }
 
   /******************************************* stack *********************************************/
-
   public static void stack( Object... objects )
   {
     // sends to standard out date-time and the input objects
     System.out.println( timestamp() + objectsString( objects ) );
 
     // sends to standard out this thread's stack trace
-    StackTraceElement[] stack = new Throwable().getStackTrace();
-    for ( int i = 1; i < stack.length; i++ )
-      System.out.println( "\t" + stackElementString( stack[i] ) );
+    STACK_WALKER.walk( stream ->
+    {
+      var iterator = stream.iterator();
+      iterator.next(); // skip stack()
+      while ( iterator.hasNext() )
+        System.out.println( "\t" + iterator.next() );
+      return null; // nothing to return
+    } );
   }
 
-  /******************************************* caller *******************************************/
-  public static String caller( int pos )
+  /**************************************** stackFrameString ****************************************/
+  private static String stackFrameString( StackFrame frame )
   {
-    // returns stack entry at specified position
-    StackTraceElement[] stack = new Throwable().getStackTrace();
-    String file = " (" + stack[++pos].getFileName() + ":" + stack[pos].getLineNumber() + ") ";
-    String method = stack[pos].getMethodName() + "()";
-    return file + method;
+    // return frame as string in desired format
+    return " (" + frame.getFileName() + ":" + frame.getLineNumber() + ") " + frame.getMethodName() + "()";
   }
 
   /**************************************** objectsString ****************************************/
@@ -102,10 +120,10 @@ public class Utils
     {
       if ( obj == null )
         str.append( "null " );
-      else if ( obj instanceof String )
-        str.append( "\"" + obj + "\" " );
-      else if ( obj instanceof Character )
-        str.append( "'" + obj + "' " );
+      else if ( obj instanceof String s )
+        str.append( '"' ).append( s ).append( "\" " );
+      else if ( obj instanceof Character c )
+        str.append( '\'' ).append( c ).append( "' " );
       else if ( obj.getClass().isArray() )
       {
         if ( obj.getClass().getComponentType().isPrimitive() )
@@ -118,13 +136,13 @@ public class Utils
             list.add( Array.get( obj, i ) );
           if ( box < len )
             list.add( "...of " + len );
-          str.append( objectsString( list ) + " " );
+          str.append( objectsString( list ) ).append( " " );
         }
         else
-          str.append( "[" + objectsString( (Object[]) obj ) + "] " );
+          str.append( "[" ).append( objectsString( (Object[]) obj ) ).append( "] " );
       }
       else
-        str.append( obj + " " );
+        str.append( obj ).append( " " );
     }
 
     // remove excess space character at end if present
@@ -133,22 +151,11 @@ public class Utils
     return str;
   }
 
-  /************************************* stackElementString **************************************/
-  public static String stackElementString( StackTraceElement element )
-  {
-    // cannot simply use stack[i].toString() because need extra space before bracket for Eclipse hyperlink to work
-    return element.getClassName() + "." + element.getMethodName()
-        + ( element.isNativeMethod() ? " (Native Method)"
-            : ( element.getFileName() != null && element.getLineNumber() >= 0
-                ? " (" + element.getFileName() + ":" + element.getLineNumber() + ")"
-                : ( element.getFileName() != null ? " (" + element.getFileName() + ")" : " (Unknown Source)" ) ) );
-  }
-
   /******************************************* clean *********************************************/
   public static String clean( String txt )
   {
     // returns a clean string
-    return txt.trim().replaceAll( "(\\s+)", " " );
+    return WHITESPACE.matcher( txt.trim() ).replaceAll( " " );
   }
 
   /******************************************** name *********************************************/
